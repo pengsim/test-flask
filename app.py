279db2466115fedba48c4e697b2df41676b062ca
pengsim <hghvb024@gmail.com>
2025-05-03 13:05:27 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+from Flask import flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+  return "<h1>Hello Welcome to Flask</h1>"
+
+if __name__ == '__main__":
+  app.run(host='0.0.0.0' , port='5000')
